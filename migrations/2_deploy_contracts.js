35511d4a2851a17f6530cc3bc7436dc117a9405d
Nathan Glover <nathan@glovers.id.au>
2018-01-23 01:00:06 +0800

    added skeleton for the trust contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ var Donation = artifacts.require('./Donation');
 var KittyCoinFactory = artifacts.require('./KittyCoinFactory');
 var KittyCoinHandler = artifacts.require('./KittyCoinHandler');
 var KittyFactory = artifacts.require('./KittyFactory');
+var TrustFactory = artifacts.require('./TrustFactory');
 
 module.exports = function (deployer) {
   deployer.deploy(Ownable);
@@ -10,4 +11,5 @@ module.exports = function (deployer) {
   deployer.deploy(KittyCoinFactory);
   deployer.deploy(KittyCoinHandler);
   deployer.deploy(KittyFactory);
+  deployer.deploy(TrustFactory);
 };
