4bf7dd7fb1b668dc2ca4a9c150e061f9d0b57594
Nathan Glover <nathan@glovers.id.au>
2018-02-01 00:37:05 +0800

    playing around with some unit tests to get my head around how web3 works

--- test/TestKittyCoinClub.js
+++ test/TestKittyCoinClub.js
@@ -1,6 +1,7 @@
-
 /* eslint no-undef: "off" */
 const KittyCoinClub = artifacts.require('../contracts/KittyCoinClub');
+
+// var errorMessage = 'Error: VM Exception while processing transaction: invalid opcode';
 
 contract('KittyCoinClub', function (accounts) {
   // ----------------------------------
@@ -9,9 +10,19 @@ contract('KittyCoinClub', function (accounts) {
   let kittycoinclub;
   const owner = accounts[0];
   const trust = accounts[1];
+  // const foster = accounts[2];
+
+  // converts strings to 32 bytes hexes
+  // function stringTo32BytesHex (string) {
+  //   var hex = web3.fromUtf8(string);
+  //   hex = (hex + '0000000000000000000000000000000000000000000000000000000000000000').slice(0, 66);
+  //   return hex;
+  // };
 
   beforeEach(async function () {
-    kittycoinclub = await KittyCoinClub.new({ from: owner });
+    kittycoinclub = await KittyCoinClub.new({
+      from: owner,
+    });
   });
 
   it('has a name', async function () {
@@ -35,15 +46,31 @@ contract('KittyCoinClub', function (accounts) {
   });
 
   it('owner can create a trusted address', async function () {
-    await kittycoinclub.createTrust(trust)
+    await kittycoinclub.createTrust(trust, {
+      from: owner,
+    })
       .then(function (result) {
         assert.include(result.logs[0].event, 'NewTrust', 'NewTrust event was not triggered');
       });
   });
 
-  // it('trust should be allowed to create a kitty', async function () {
-  //   await kittycoinclub.createKitty(foster, '0x00f9e605e3', 0.003)
+  // it('trust address is actually a trust', async function () {
+  //   await kittycoinclub.isTrustAddress(trust, {
+  //     from: owner,
+  //   })
   //     .then(function (result) {
+  //       assert.include(result, 'true ', 'trust address is not a trust');
+  //     });
+  // });
+
+  // it('trust should be allowed to create a kitty', async function () {
+  //   var price = web3.toWei(3, 'ether');
+  //   var seed = stringTo32BytesHex('0x00f9e605e3');
+  //   await kittycoinclub.createKitty(foster, seed, price, {
+  //     from: trust,
+  //   })
+  //     .then(function (result) {
+  //       console.log(result);
   //       assert.include(result.logs[0].event, 'NewKitty', 'NewKitty event was not triggered');
   //     });
   // });
